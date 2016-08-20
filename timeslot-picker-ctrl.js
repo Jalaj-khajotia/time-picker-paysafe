3e563632157d8da1196aca895634deac272125df
jalaj <jalajiitr@gmail.com>
2016-08-21 00:54:18 +0530

    initial state

---
+++ timeslot-picker-ctrl.js
@@ -0,0 +1,125 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var app = angular.module('paysafe-assignment', ['ngStorage']);
+
+app.controller('timeslot-pickerCtrl', function($scope, $http, $localStorage) {
+
+    var index = 0;
+    $scope.amSlotSelected = true;
+
+    if ($localStorage.jsonData) {
+        $scope.slotsData = $localStorage.jsonData;
+        reset();
+    } else {
+        $http.get('testdata.json').then(function(res) {
+            console.info(res);
+            $scope.slotsData = res.data;
+            $localStorage.jsonData = res.data;
+            reset();
+        }, function(error) {
+            console.error(error);
+        });
+    }
+
+
+    $scope.toggleTime = function(slotType) {
+        if (slotType == 'am') {
+            $scope.amSlotSelected = true;
+        } else {
+            $scope.amSlotSelected = false;
+        }
+        reset();
+    }
+
+    $scope.resetUnload = function() {
+        $scope.amSlotSelected = true;
+        index = 0;
+        reset();
+    }
+
+    function reset() {
+        var slotType, i = 0,
+            end = 0;
+        if ($scope.amSlotSelected) {
+            slotType = 'am';
+            i = 0;
+            end = 12;
+        } else {
+            slotType = 'pm';
+            i = 12;
+            end = 24;
+        }
+        $scope.date = $scope.slotsData[index].date;
+
+        $scope.timeselection = [];
+        for (var j = 1; i < end; i++, j++) {
+            var obj = $scope.slotsData[index].slots[i];
+            obj.selector = 'li' + j;
+            $scope.timeselection.push(obj);
+        }
+        console.info($scope.timeselection);
+    }
+    $scope.count = 1;
+    var selectedIndex = 0;
+    $scope.bookSlot = function(index) {
+        var element = $scope.timeselection[index];
+        if (element.status === 'available') {
+            element.status = 'selected';
+        } else if (element.status === 'selected') {
+            element.status = 'available';
+        }
+    }
+    $scope.confirmSlot = function() {
+        alert('your slot has been confimed');
+        $scope.timeselection.forEach(function(element) {
+            if (element.status == 'selected') {
+                element.status = 'reserved';
+            }
+        }, this);
+
+        if ($scope.amSlotSelected) {
+            // $scope.slotsData[index].slots.splice(0, 12);
+            var j = 0;
+            $scope.timeselection.forEach(function(element) {
+                $scope.slotsData[index].slots[j] = $scope.timeselection[j];
+                j++;
+            }, this);
+            //   $scope.slotsData[index].slots.unshift($scope.timeselection);
+        } else {
+            var j = 12,
+                i = 0;
+            $scope.timeselection.forEach(function(element) {
+                $scope.slotsData[index].slots[j] = $scope.timeselection[i];
+                i++;
+                j++
+            }, this);
+        }
+        $localStorage.jsonData = $scope.slotsData;
+    }
+
+    $scope.shiftDateLeft = function() {
+        if (index > 0) {
+            index--;
+            reset();
+        }
+    }
+    $scope.shiftDateRight = function() {
+        if (index < $scope.slotsData.length - 1) {
+            index++;
+            reset();
+        }
+    }
+});
+/*.
+
+    config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+        // $locationProvider.hashPrefix('!');
+
+        $routeProvider.when('/home', {
+            templateUrl: 'home/views/home.html',
+            controller: 'HomeCtrl'
+        });
+        $routeProvider.otherwise({redirectTo: '/home'});
+    }]);
+*/
